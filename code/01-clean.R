b548052380094cf34e889c919146274845c4f044
mz <maja.zaloznik@gmail.com>
2020-11-21 16:30:06 +0100

    change data source & add 7day rolling average

--- code/01-clean.R
+++ code/01-clean.R
@@ -5,10 +5,8 @@
 library(readr)
 library(dplyr)
 library(tidyr)
-library(stringr)
-library(tidyjson) 
 library(lubridate)
-
+library(zoo)
 ###############################################################################
 
 ## import data
@@ -18,45 +16,41 @@ library(lubridate)
 # download.file(paste0("https://raw.githubusercontent.com/",
 #                      "sledilnik/data/master/csv/stats.csv"),
 #               "data/stats.csv")
-# download.file(paste0("https://raw.githubusercontent.com/",
-#                      "sledilnik/data/master/ostanizdrav/podatki.gov.si/tanstatistika.csv"),
-#               "data/issued.csv")
 # 
 # download.file(paste0("https://raw.githubusercontent.com/",
-#                      "sledilnik/data/master/ostanizdrav/ctt/data.json"),
-#               "data/published.json")
+#                      "sledilnik/data/master/ostanizdrav/merged.csv"),
+#               "data/merged.csv")
 
 confirmed.cases <- read_csv("data/stats.csv")
 
-tan.issued <- read_csv("data/issued.csv",
-                       col_types = 
-                         cols_only('created_at' = col_date("%d.%m.%Y"), 
-                                   'count' = col_double())) 
 
-tan.published <- fromJSON("data/published.json")
-    
-    
+ostani.zdrav <-   read_csv("data/merged.csv")
+
 ## clean up data
 ###############################################################################
 
 confirmed.cases %>% 
   select(date, tests.positive) ->   confirmed.cases
 
-tan.published %>% 
-  mutate(date = as.Date(date, "%Y-%m-%d")) -> tan.published
-
-tan.issued %>% 
-  rename("date" = "created_at") -> tan.issued
-
-left_join(tan.published, confirmed.cases) %>% 
-  left_join(tan.issued) -> df 
+confirmed.cases %>% 
+  right_join(ostani.zdrav, by = c("date" = "Date")) %>% 
+  rename(issued = TAN.issued,
+         published = TAN.users, 
+         users = downloads.toDate,
+         confirmed = tests.positive) -> df
+             
 
 ## calculate stuff 
 ###############################################################################
 
 df %>% 
-  mutate(issued.confirmed = count/tests.positive* 100,
-         publihsed.confirmed = users_published/tests.positive*100 ) -> df
+  mutate(issued.7d = rollmean(issued, 7, align = "right", na.pad = TRUE),
+         published.7d = rollmean(published, 7, align = "right", na.pad = TRUE),
+         confirmed.7d = rollmean(confirmed, 7, align = "right", na.pad = TRUE),
+         issued.confirmed = issued/confirmed * 100,
+         publihsed.confirmed = published/confirmed*100 ,
+         issued.confirmed.7d = issued.7d/confirmed.7d * 100,
+         publihsed.confirmed.7d = published.7d/confirmed.7d *100 ) -> df
 
 
 
@@ -90,5 +84,40 @@ mtext(side = 2, line = 2.5,   cex = 1,
       "percent")
 mtext(side = 1, line = 2.5,   cex = 1, "date")
 dev.off()
+
+
+# plot ratio of  issued to positive 7 day rolling average 
+###############################################################################
+
+png(filename="figures/TAN.vs.confirmed.7d.png", 800, 480)
+plot(df$date, df$issued.confirmed.7d, 
+     type = "l",
+     ylab = "",
+     xlab = "", 
+     ylim = c(0,20),
+     xlim = as.Date(c("2020-08-19", "2020-11-20")),
+     axes = FALSE,
+     panel.first={
+       abline(v = as.Date("2020-10-26"), lty = "92", col = "darkgrey")
+     })
+
+axis.Date(1, df$date, at = seq(min(df$date), max(df$date), by="7 day"))
+axis(2, las = 2, labels = )
+lines(df$date, df$publihsed.confirmed.7d, col = "blue")
+abline(h = 10, lty = 2, col = "gray")
+abline(h = 15, lty = 2, col = "gray")
+abline(h = 5, lty = 2, col = "gray")
+abline(h = 20, lty = 2, col = "gray")
+
+mtext(side = 3, line = 2.5,  adj = 0, cex = 1.2,
+      "Percentage of daily confirmed cases who were issued with TAN codes (black) and who published TAN codes (blue)")
+mtext(side = 3, line = 1.5,  adj = 0, cex = 1,
+      "7 day rolling average")
+
+mtext(side = 2, line = 2.5,   cex = 1, 
+      "percent")
+mtext(side = 1, line = 2.5,   cex = 1, "date")
+dev.off()
+
 
 
